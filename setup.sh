846b17f76b5baf3e275ef851ae9b616d9030ed1c
joncx0 <78873415+joncx0@users.noreply.github.com>
2021-06-03 10:22:42 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,93 +4,80 @@ mkdir dependencies ; mkdir tools
 echo "[OK]"
 
 echo -ne "Install dependencies..."
-cd /root/dependencies > /dev/null 2>&1 
-apt install git -y > /dev/null 2>&1
-apt install curl -y > /dev/null 2>&1
-apt install python -y > /dev/null 2>&1
-apt install python3 -y > /dev/null 2>&1
-apt install make -y > /dev/null 2>&1 
-apt install zip -y > /dev/null 2>&1
-wget https://bootstrap.pypa.io/get-pip.py > /dev/null 2>&1
-mv get-pip.py pip3.py > /dev/null 2>&1
-python3 pip3.py > /dev/null 2>&1
-wget https://bootstrap.pypa.io/pip/2.7/get-pip.py > /dev/null 2>&1
-mv get-pip.py pip.py > /dev/null 2>&1
-python pip.py > /dev/null 2>&1
+cd /root/dependencies
+apt install git -y
+apt install curl -y 
+apt install python -y 
+apt install python3 -y 
+apt install make -y  
+apt install zip -y 
+wget https://bootstrap.pypa.io/get-pip.py 
+mv get-pip.py pip3.py 
+python3 pip3.py 
+wget https://bootstrap.pypa.io/pip/2.7/get-pip.py 
+mv get-pip.py pip.py 
+python pip.py
 rm pip.py pip3.py
 wget https://golang.org/dl/go1.16.4.linux-amd64.tar.gz ; tar xvf go1.16.4.linux-amd64.tar.gz ; chown -R root:root ./go ; mv go /usr/local 
 echo "export GOPATH=$HOME/go" >> /root/.bashrc
 echo "export PATH=$PATH:/usr/local/go/bin:$GOPATH/bin" >> /root/.bashrc
-source /root/.bashrc > /dev/null 2>&1
+source /root/.bashrc 
 cd
 echo "[OK]"
 
 echo -ne "Install go tools..."
-cd /root/tools > /dev/null 2>&1
-go get -u github.com/tomnomnom/anew > /dev/null 2>&1 
-go get -u github.com/tomnomnom/assetfinder > /dev/null 2>&1 
-go get -u github.com/ffuf/ffuf > /dev/null 2>&1 
-go get -u github.com/tomnomnom/gf > /dev/null 2>&1
-go get github.com/hakluke/hakrawler > /dev/null 2>&1
-go get -v github.com/projectdiscovery/httpx/cmd/httpx > /dev/null 2>&1
-go get github.com/jaeles-project/jaeles > /dev/null 2>&1
-go get -v github.com/projectdiscovery/nuclei/v2/cmd/nuclei > /dev/null 2>&1 
-go get github.com/Emoe/kxss > /dev/null 2>&1
-go get -v github.com/projectdiscovery/naabu/v2/cmd/naabu > /dev/null 2>&1
-go get -u github.com/tomnomnom/qsreplace > /dev/null 2>&1
-go get -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder > /dev/null 2>&1
-go get github.com/tomnomnom/waybackurls > /dev/null 2>&1
-go get github.com/003random/getJS > /dev/null 2>&1
+cd /root/tools
+go get -u github.com/tomnomnom/anew ; go get -u github.com/tomnomnom/assetfinder ; go get -u github.com/ffuf/ffuf ; go get -u github.com/tomnomnom/gf ; go get github.com/hakluke/hakrawler ; go get -v github.com/projectdiscovery/httpx/cmd/httpx ; go get github.com/jaeles-project/jaeles ; go get -v github.com/projectdiscovery/nuclei/v2/cmd/nuclei ; go get github.com/Emoe/kxss ; go get -v github.com/projectdiscovery/naabu/v2/cmd/naabu ; go get -u github.com/tomnomnom/qsreplace ; go get -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder ; go get github.com/tomnomnom/waybackurls ; go get github.com/003random/getJS
 echo "[OK]"
 
 echo -ne "Clone git tools..."
 cd tools > /dev/null 2>&1
-git clone https://github.com/tomnomnom/hacks.git > /dev/null 2>&1
-wget https://github.com/hahwul/dalfox/releases/download/v2.3.7/dalfox_2.3.7_linux_amd64.tar.gz > /dev/null 2>&1
-tar xvf dalfox_2.3.7_linux_amd64.tar.gz > /dev/null 2>&1 
-rm LICENSE.txt README.md > /dev/null 2>&1 
-rm dalfox_2.3.7_linux_amd64.tar.gz > /dev/null 2>&1
-wget https://github.com/Findomain/Findomain/releases/download/4.2.0/findomain-linux > /dev/null 2>&1
-mv findomain-linux findomain > /dev/null 2>&1
-chmod +x findomain > /dev/null 2>&1
-wget https://github.com/brentp/gargs/releases/download/v0.3.9/gargs_linux > /dev/null 2>&1
-mv gargs_linux gargs > /dev/null 2>&1 
-chmod +x gargs > /dev/null 2>&1
-git clone https://github.com/bp0lr/gauplus.git > /dev/null 2>&1
-mv gauplus gauplus2 > /dev/null 2>&1
-cd gauplus2 > /dev/null 2>&1
-go build main.go > /dev/null 2>&1
-mv main gauplus > /dev/null 2>&1
-cp gauplus /bin > /dev/null 2>&1
-cd /root/tools > /dev/null 2>&1
-git clone https://github.com/jaeles-project/jaeles-signatures.git > /dev/null 2>&1
-git clone https://github.com/blechschmidt/massdns.git > /dev/null 2>&1 
-cd massdns > /dev/null 2>&1 
-make > /dev/null 2>&1 
-cd bin > /dev/null 2>&1 
-cp massdns /bin > /dev/null 2>&1 
-cd /root/tools > /dev/null 2>&1 > /dev/null 2>&1
-git clone https://github.com/projectdiscovery/nuclei-templates.git > /dev/null 2>&1
-git clone https://github.com/danielmiessler/SecLists.git > /dev/null 2>&1
-git clone https://github.com/d3mondev/puredns.git > /dev/null 2>&1
-cd puredns > /dev/null 2>&1
-go build main.go > /dev/null 2>&1
-mv main puredns > /dev/null 2>&1
-cp puredns /bin > /dev/null 2>&1
-cd /root/tools > /dev/null 2>&1
-git clone https://github.com/joncx0/ptf.git > /dev/null 2>&1
-cd /root/tools/ptf > /dev/null 2>&1
-wget https://raw.githubusercontent.com/joncx0/install/main/custom_ptf.txt > /dev/null 2>&1
-cp custom_ptf.txt /root/tools/ptf/modules/custom_list/ > /dev/null 2>&1
-cd /root/tools/ptf > /dev/null 2>&1
+git clone https://github.com/tomnomnom/hacks.git 
+wget https://github.com/hahwul/dalfox/releases/download/v2.3.7/dalfox_2.3.7_linux_amd64.tar.gz 
+tar xvf dalfox_2.3.7_linux_amd64.tar.gz 
+rm LICENSE.txt README.md 
+rm dalfox_2.3.7_linux_amd64.tar.gz 
+wget https://github.com/Findomain/Findomain/releases/download/4.2.0/findomain-linux 
+mv findomain-linux findomain 
+chmod +x findomain 
+wget https://github.com/brentp/gargs/releases/download/v0.3.9/gargs_linux 
+mv gargs_linux gargs
+chmod +x gargs 
+git clone https://github.com/bp0lr/gauplus.git 
+mv gauplus gauplus2 
+cd gauplus2 
+go build main.go 
+mv main gauplus 
+cp gauplus /bin 
+cd /root/tools 
+git clone https://github.com/jaeles-project/jaeles-signatures.git 
+git clone https://github.com/blechschmidt/massdns.git 
+cd massdns 
+make 
+cd bin 
+cp massdns /bin 
+cd /root/tools 
+git clone https://github.com/projectdiscovery/nuclei-templates.git 
+git clone https://github.com/danielmiessler/SecLists.git 
+git clone https://github.com/d3mondev/puredns.git 
+cd puredns
+go build main.go
+mv main puredns
+cp puredns /bin 
+cd /root/tools
+git clone https://github.com/joncx0/ptf.git
+cd /root/tools/ptf
+wget https://raw.githubusercontent.com/joncx0/install/main/custom_ptf.txt
+cp custom_ptf.txt /root/tools/ptf/modules/custom_list/
+cd /root/tools/ptf
 ./ptf
 use modules/custom_list/custom_ptf
 yes
 echo "[OK]"
 
 echo -ne "Add bin directory..."
-cp /root/tools dalfox findomain gargs gauplus /root/go/bin > /dev/null 2>&1
-cp /root/go/bin/* /bin > /dev/null 2>&1
+cp /root/tools dalfox findomain gargs gauplus /root/go/bin  2>&1
+cp /root/go/bin/* /bin 
 echo "[OK]"
 
 echo "[Let's hack!]"
